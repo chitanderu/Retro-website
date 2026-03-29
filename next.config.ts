525b7a6d20a08caea7407c8cfc26be991ddc75ff
Chitanderu <982653387@qq.com>
2026-03-29 16:26:37 +0800

    feat: add Dockerfile and .dockerignore; configure next.config for basePath and assetPrefix

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  basePath: "/hyouka",
+  assetPrefix: "/hyouka",
+  output: "standalone",
   images: {
     remotePatterns: [
       {
